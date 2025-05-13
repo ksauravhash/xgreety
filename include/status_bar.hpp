cf3654c94de26fa794bfa3e129c7f84046debfe2
Saurav Kumar <ksauravhash@gmail.com>
2025-05-13 09:06:31 +0530

    basic conf

--- include/status_bar.hpp
+++ include/status_bar.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
+#include <vector>
 #include "window.hpp"
 namespace xgreety {
 class StatusBar : public Window {
+  private:
+  short int currentActive = 0;
+  std::vector<short int> div_ratios;
+  std::vector<short int> sizes;
+
+  void drawText();
+  void drawDate();
+  void drawTime();
  public:
   StatusBar();
   StatusBar(int nlines, int ncols, int begin_y, int begin_x);
+
+  void configure();
 
   void draw();
   void handleInput(int ch);
--- src/status_bar.cpp
+++ src/status_bar.cpp
@@ -1,5 +1,9 @@
 #include "status_bar.hpp"
 
+#include <ncurses.h>
+
+#include <vector>
+
 #include "window.hpp"
 
 namespace xgreety {
@@ -7,8 +11,31 @@ StatusBar::StatusBar() {}
 StatusBar::StatusBar(int nlines, int ncols, int begin_y, int begin_x)
     : Window(nlines, ncols, begin_y, begin_x) {}
 
-void StatusBar::draw() {}
+void StatusBar::configure() {
+  keypad(win, true);
+  noecho();
+  div_ratios.push_back(1);
+  div_ratios.push_back(2);
+  div_ratios.push_back(1);
+
+  int ratios_sumed = 0;
+  for (short int i : div_ratios) ratios_sumed += i;
+  for (size_t i = 0; i < div_ratios.size(); i++) sizes.push_back(xMax / div_ratios[i]);
+}
+
+void StatusBar::draw() {
+    drawText();
+    drawDate();
+    drawTime();
+}
 
 void StatusBar::handleInput(int ch) {}
+
+void StatusBar::drawText() {
+}
+
+void StatusBar::drawDate() {}
+
+void StatusBar::drawTime() {}
 
 }  // namespace xgreety
